72ad141c1e892de8d19c8c3b618edd734fc235cd
Bhatmanju307 <90103579+Bhatmanju307@users.noreply.github.com>
2023-01-01 10:15:27 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,9 +9,9 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5775731292:AAFZIznwzhvQs2s53iyqXl8NsIWWqZVe6cE")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "MdiskSearchBot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -1001831635652))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Sserials_search_bot")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "1158419686"))
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     ABOUT_BOT_TEXT = """<b>This is Mdisk Search Bot.
 
